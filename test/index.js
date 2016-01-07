df3ccf8fc54aad7453bde144dd34fbfa6bc68bd1
jongleberry <me@jongleberry.com>
2016-01-06 17:21:57 -0800

    test: write results

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const babel = require('babel-core')
+const mkdirp = require('mkdirp')
+const rimraf = require('rimraf')
 const path = require('path')
 const fs = require('fs')
 
@@ -12,6 +14,10 @@ const presets = [
 
 const fixtures = fs.readdirSync(path.join(__dirname, 'fixtures'))
 
+before(done => {
+  rimraf(path.join(__dirname, 'results'), done)
+})
+
 describe('ES2016 + Flow', () => {
   presets.forEach(name => {
     describe(name, () => {
@@ -19,6 +25,11 @@ describe('ES2016 + Flow', () => {
       fixtures.forEach(fixture => {
         it(fixture, () => {
           const result = babel.transform(fs.readFileSync(path.join(__dirname, 'fixtures', fixture), 'utf8'), preset)
+
+          const pathname = path.join(__dirname, 'results', name, fixture)
+          mkdirp.sync(path.dirname(pathname))
+          fs.writeFileSync(pathname, result.code)
+
           new Function(result.code)
         })
       })
