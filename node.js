413e5d9c0628f3c072b1a3962f68669d364cd052
Jonathan Ong <jonathanrichardong@gmail.com>
2016-01-11 18:47:34 -0800

    use babel-features instead of node presets

--- node.js
+++ node.js
@@ -1,11 +1,18 @@
 'use strict'
 
 const version = ~~/^v(\d+)/.exec(process.version)[1]
+if (version < 4) throw new Error('Only node v4+ is supported!')
 
 const wrapDefault = require('./wrap-default')
 
 module.exports = {
   plugins: [
+    [
+      require('babel-plugin-transform-async-to-module-method'), {
+        module: 'bluebird',
+        method: 'coroutine'
+      }
+    ],
     [
       wrapDefault(require('babel-plugin-typecheck')), {
         disable: {
@@ -16,11 +23,13 @@ module.exports = {
     require('babel-plugin-syntax-flow'),
     require('babel-plugin-transform-flow-strip-types'),
     // require('babel-plugin-transform-runtime'),
-  ],
+  ].concat(require('babel-features').options().plugins.filter(x => {
+    // don't use regenerator because it sucks
+    if (x === 'transform-regenerator') return false
+    return true
+  }).map(name => require('babel-plugin-' + name))),
+
   presets: [
     require('babel-preset-stage-0'),
-    version >= 5 ? require('babel-preset-es2015-node5')
-      : version >= 4 ? require('babel-preset-es2015-node4')
-      : require('babel-preset-es2015'),
   ],
 }
