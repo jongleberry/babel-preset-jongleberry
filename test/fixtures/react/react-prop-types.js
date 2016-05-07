411d9b3ea0e30a49454ba767995b1dfcd951b3f4
Jonathan Ong <jonathanrichardong@gmail.com>
2016-05-07 16:28:25 -0700

    add react prop types test case

---
+++ test/fixtures/react/react-prop-types.js
@@ -0,0 +1,12 @@
+
+import React, { Component, PropTypes } from 'react'
+
+export default class Something extends Component {
+  static propTypes = {
+    className: PropTypes.string
+  };
+
+  render () {
+    return <div />
+  }
+}
